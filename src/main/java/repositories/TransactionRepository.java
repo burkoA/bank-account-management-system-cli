047c12912743c1eaa5bda0befe1e588cc939973e
Arsen Burko <arsenburko67@gmail.com>
2026-01-28 13:30:13 +0100

    Implement transaction history & refactor code

---
+++ src/main/java/repositories/TransactionRepository.java
@@ -0,0 +1,42 @@
+package repositories;
+
+import model.Transaction;
+import model.User;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionRepository {
+
+    private static final String TRANSACTION_FILE = "transaction.json";
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private List<Transaction> transactions;
+
+    public TransactionRepository() {
+        this.transactions = readTransaction();
+    }
+
+    private List<Transaction> readTransaction() {
+        File file = new File(TRANSACTION_FILE);
+
+        if (!file.exists() || file.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        return objectMapper.readValue(file, new TypeReference<List<Transaction>>() {});
+    }
+
+    private void writeTransactions() {
+        objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValue(new File(TRANSACTION_FILE), transactions);
+    }
+
+
+    public void createTransaction(Transaction transaction) {
+        transactions.add(transaction);
+        writeTransactions();
+    }
+}
